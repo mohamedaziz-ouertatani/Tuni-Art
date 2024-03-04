c02fce4e83bdd3901e102424e1e6bae11dccb62b
Restyled.io <commits@restyled.io>
2024-03-04 13:45:23 +0000

    Restyled by php-cs-fixer

--- src/main/java/tn/esprit/gui/addDeliveries.php
+++ src/main/java/tn/esprit/gui/addDeliveries.php
@@ -1,4 +1,5 @@
 <?php
+
 // Connect to the database
 $servername = "localhost";
 $username = "root";
@@ -9,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // Get the agency name from the POST data
@@ -17,16 +18,16 @@ $agencyName = $_POST['agencyName'];
 
 // Add the delivery to the table
 $sql = "INSERT INTO deliveries (agency_name) VALUES ('$agencyName')";
-if ($conn->query($sql) === TRUE) {
-  // Increase the deliveries number for the selected agency
-  $sql = "UPDATE delivery_agency SET nb_deliveries = nb_deliveries + 1 WHERE agency_name = '$agencyName'";
-  if ($conn->query($sql) === TRUE) {
-    echo 'Delivery added successfully';
-  } else {
-    echo 'Error adding delivery: ' . $conn->error;
-  }
+if ($conn->query($sql) === true) {
+    // Increase the deliveries number for the selected agency
+    $sql = "UPDATE delivery_agency SET nb_deliveries = nb_deliveries + 1 WHERE agency_name = '$agencyName'";
+    if ($conn->query($sql) === true) {
+        echo 'Delivery added successfully';
+    } else {
+        echo 'Error adding delivery: ' . $conn->error;
+    }
 } else {
-  echo 'Error adding delivery: ' . $conn->error;
+    echo 'Error adding delivery: ' . $conn->error;
 }
 
 // Close the connection
